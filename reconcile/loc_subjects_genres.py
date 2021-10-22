399bb9584e52a0629b121d039ef3974d488e171f
rlskoeser <rebecca.s.koeser@princeton.edu>
2021-10-22 08:45:11 -0400

    Update to use SRU and handle 500 errors

--- reconcile/loc_subjects_genres.py
+++ reconcile/loc_subjects_genres.py
@@ -3,8 +3,10 @@
 
 import csv
 from time import sleep
+import os.path
 
 import requests
+from requests.models import LocationParseError
 from eulxml import xmlmap
 from eulxml.xmlmap import load_xmlobject_from_string
 from eulxml.xmlmap.mods import MODS, Subject
@@ -20,37 +22,98 @@ class LoCMODS(MODS):
     # override with our subject class
     subjects = xmlmap.NodeListField("mods:subject", LoCSubject)
 
+class SearchRetrieveResponse(MODS):
+    # extend MODS to inherit the mods namespace (cheat)
+
+    # zs namespace if we need it
+    # xmlns:zs="http://www.loc.gov/zing/srw/"
+    # zs:records/zs:record/zs:recordData/mods
+    mods = xmlmap.NodeField("//mods:mods", LoCMODS)
+
 
 # Item and resource endpoints:
 # Burst Limit:  	40 requests per 10 seconds, Block for 5 minutes
 # Crawl Limit  	200 requests per 1 minute, Block for 1 hour
 # ... not sure if these limits apply to MODS record
 
+# use a request session for connection pooling
+session = requests.Session()
+session.headers.update({
+       'User-Agent': 'loc-reconcler 0.1 / Center for Digital Humanities at Princeton',
+        'From': 'rkoeser@princeton.edu'
+    })
+
 @sleep_and_retry
-@limits(calls=35, period=10)
-def get_loc_mods(loc_id):
+# @limits(calls=35, period=10)
+@limits(calls=15, period=10)
+def get_loc_mods(loc_id, retry=0):
     # MODS xml can be retrieved relative to main loc id
-    response = requests.get(f"{ loc_id }/mods")
-    if response.status_code >= 400:
-        print("😱 got a %s error, trying again" % response.status_code)
-        sleep(1)
-        return get_loc_mods(loc_id)
-    return load_xmlobject_from_string(response.content, LoCMODS)
+    # BUT: that seems to cause problems on their server! results in 403s
+    # use SRU endpoint instead
+    lccn_id = loc_id.rstrip('/').split('/')[-1]
+    sru_url = f"http://lx2.loc.gov:210/LCDB?operation=searchRetrieve&version=1.1&query=bath.lccn=%22^{ lccn_id }$%22&recordSchema=mods&startRecord=1&maximumRecords=10"
+    # sru_url = f"http://lx2.loc.gov:210/lcdb?version=1.1&operation=searchRetrieve&query={ lccn_id }&recordSchema=mods&maximumRecords=1"
+    response = session.get(sru_url)
+    if response.status_code == requests.codes.ok:
+        sru_response = load_xmlobject_from_string(response.content, SearchRetrieveResponse)
+        mods = sru_response.mods
+        if not mods:
+            print('no mods %s' % sru_url)
+        return mods
+    else:
+        if response.status_code == 500 and retry < 3:
+            print("😱 got a %s error, trying again (try %s)" % (response.status_code, retry + 1))
+            sleep(3*60)
+            return get_loc_mods(loc_id, retry=retry + 1)
+
+        print(response.status_code)
+        print(response.content)
+        # bail out
+        raise Exception
+
+
+# if csv output file exists, load it and make a list of sco ids with existing info
+# append to file & skip writing header if it already existed
+output_filename = "shxco_loc_subjects_genres.csv"
+has_genre_info = []
+
+if os.path.exists(output_filename):
+    with open(output_filename) as csvfile:
+        csvreader = csv.DictReader(csvfile)
+        has_genre_info = set([row["sco_id"] for row in csvreader])
+
+if has_genre_info:
+    print("loaded %d records with genre/subject info from csv file" % len(has_genre_info))
 
 
 with open("shxco_loc_matches_final.csv") as csvfile:
     csvreader = csv.DictReader(csvfile)
 
     csv_fieldnames = ["sco_id", "term", "type", "source"]
-    with open("shxco_loc_subjects_genres.csv", "w") as csvfile:
+    with open(output_filename, "a") as csvfile:
         csvwriter = csv.DictWriter(csvfile, fieldnames=csv_fieldnames)
+        # only write out the header if we don't have existing info (i.e. new file)
+        if not has_genre_info:
+            csvwriter.writeheader()
 
         for row in csvreader:
+            # skip if we've already gotten subject/genre info for this one
+            if row["sco_id"] in has_genre_info:
+                continue
+
             loc_id = row["id"]
             # print id as an indicator of where we are
             print("%s - %s" % (row["sco_id"].split("/")[-2], loc_id))
             # use a method to load mods so we can apply rate limiting
-            mods = get_loc_mods(loc_id)
+            try:
+                mods = get_loc_mods(loc_id)
+            except:
+                # if we start getting 500s, break so the file will get closed
+                break
+
+            if not mods:
+                continue
+
             # get unique subjects & genres
             genre_list = set()
             subject_list = set()
